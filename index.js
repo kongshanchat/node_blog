1f79add65bdef30255c058a22f24d8ac6d818da6
yangyang <1561043491@qq.com>
2018-09-04 14:45:03 +0800

    ss

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // index.js
 const Koa = require('koa')
 const router = require('./routes')
+
 const path = require('path')
 const app = new Koa()
 const views = require('koa-views')
-router(app)
+
 app.use(views(path.join(__dirname, 'views'), {
     map: { html: 'nunjucks' }
 }))
 
-app.listen(3000, () => {
-  console.log('server is running at http://localhost:3000')
+router(app)
+
+app.listen(3003, () => {
+  console.log('server is running at http://localhost:3003')
 })
--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,21 @@
-const router = require('koa-router')();
-async function isLoginUser(ctx,next){
-    if(!ctx.session.user){
-        ctx.flash = { warning:'未登录,请先登录'}
-        return ctx.redirect('/signin')
-    }
-    await next()
+// routes/index.js
+const router = require('koa-router')()
+
+module.exports = (app) => {
+  router.get('/', require('./home').index)
+  router.get('/about', require('./about').index)
+
+  app
+    .use(router.routes())
+    .use(router.allowedMethods())
 }
 
-async function isAdmin(ctx,user){
-    if(!ctx.session.user){
-        ctx.flash = { warning:'未登录,请先登录'};
-        return ctx.redirect('/signin')
-    }
-    if(!ctx.session.user.isAdmin){
-        ctx.flash = {warning:'没有权限'};
-        return ctx.redirect('/back');
-    }
-    await next();
-}
-
-module.exports=(app)=>{
-    router.get('/',require('./posts').index);
-    router.get('/about',require('./about').index);
-    router.get('/user/:username',require('./user').index);
-    router.get('/signup',require('./user').signup);
-    router.post('/signup',require('./user').signup);
-    router.get('/signin', require('./user').signin)
-  router.post('/signin', require('./user').signin)
-  router.get('/signout', isLoginUser, require('./user').signout)
-  router.get('/posts/new', isLoginUser, require('./posts').create)
-  router.post('/posts/new', isLoginUser, require('./posts').create)
-  router.get('/posts', require('./posts').index)
-  router.get('/posts/:id', require('./posts').show)
-  router.get('/posts/:id/edit', isLoginUser, require('./posts').edit)
-  router.post('/posts/:id/edit', isLoginUser, require('./posts').edit)
-  router.get('/posts/:id/delete', isLoginUser, require('./posts').destroy)
-  router.post('/comments/new', isLoginUser, require('./comments').create)
-  router.get('/comments/:id/delete', isLoginUser, require('./comments').destroy)
-  router.get('/category', isAdmin, require('./category').list)
-  router.get('/category/new', isAdmin, require('./category').create)
-  router.post('/category/new', isAdmin, require('./category').create)
-  router.get('/category/:id/delete', isAdmin, require('./category').destroy)
-
-  app.use(router.routes());
-  app.use(router.allowedMethods());
-
-//404
-app.use(async (ctx,next)=>{
-    await ctx.render('404',{
-        title:'page not found'
+// routes/home.js
+module.exports = {
+  async index (ctx, next) {
+    await ctx.render('index', {
+      title: 'abc-blog',
+      desc: '欢迎关注公众号 JavaScript之禅'
     })
-})
+  }
 }
-
